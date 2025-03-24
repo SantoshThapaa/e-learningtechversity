386db663569ef03f48ec6ccf8ad33c98d4cf27c1
SantoshThapaa <st708611@gmail.com>
2025-03-24 16:41:16 +0545

    Course route modiifed

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { getAllCourses, getSingleCourse } from '../controllers/course.js';
 
 const router = express.Router();
 
-// router.get("/courses/all", getAllCourses);
+router.get("/course/all", getAllCourses);
+router.get("/course/:id", getSingleCourse);
 
 export default router;
