934631fb17ab5dcd59fa29201548fb80a5658621
SantoshThapaa <st708611@gmail.com>
2025-03-24 16:40:15 +0545

    Teacher controller addded

--- server/controllers/teacher.js
+++ server/controllers/teacher.js
@@ -1,7 +1,12 @@
 import TryCatch from "../middlewares/TryCatch.js";
 
 export const createCourse = TryCatch(async(req, res)=>{
-    const {title, description, category, createdBy, duration, price }= req.body;
+    const {title,
+        description,
+        category,
+        createdBy,
+        duration,
+        price}= req.body;
     const image = req.file;
     await courses.create({
         title,
@@ -14,5 +19,29 @@ export const createCourse = TryCatch(async(req, res)=>{
     });
     res.status(201).json({
         message: "Course created successfully"
-    })
+    });
+});
+
+
+export const addLectures = TryCatch(async(req, res)=>{
+    const course = await Courses.findById(req.params.id)
+    if(!course)
+        return res.status(404).json({
+    message: "No course with this id",
+});
+const {title, description }= req.body;
+
+const file = req.file;
+
+const lecture = await lecture.create({
+    title,
+    description,
+    video: file?.path,
+    course: course._id,
+});
+
+return res.status(201).json({
+    message:"Lecture added successfully",
+    lecture,
+})
 })
