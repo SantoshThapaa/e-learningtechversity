a75712d7d05170b8d668f7d1d901db11fe270448
SantoshThapaa <st708611@gmail.com>
2025-03-24 16:39:42 +0545

    Course controller modified

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -1,8 +1,24 @@
 import TryCatch from "../middlewares/TryCatch.js";
-import { Course } from "../models/Lecture.js";
+import { Courses } from "../models/Courses.js";
+import { Lecture } from "../models/Lecture.js";
+import { User } from "../models/User.js";
+
+export const getAllCourses = TryCatch(async (req, res) => {
+    const courses = await Courses.find();
+    res.json({
+        courses,
+    });
+});
+
+export const getSingleCourse = TryCatch(async(req, res)=>{
+    const course = await Courses.findById(req.params.id)
+    res.json({
+        course,
+    })
+})
 
 export const addCourses = TryCatch(async (req, res) => {
-    const course = await Course.findById(req.params.id); 
+    const course = await Courses.findById(req.params.id); 
     if (!course)
         return res.status(404).json({
             message: "No Course with this id",
@@ -11,7 +27,7 @@ export const addCourses = TryCatch(async (req, res) => {
     const { title, description } = req.body;
     const file = req.file; 
     const { videoLink } = req.body;  
-    const lecture = await Course.create({
+    const lecture = await Courses.create({
         title,
         description,
         video: videoLink, 
@@ -22,3 +38,11 @@ export const addCourses = TryCatch(async (req, res) => {
         message: "Lecture Added"
     });
 });
+
+// export const fetchLectures = TryCatch(async(req, res)=>{
+//     const lectures = await Lecture.find({course: req.params.id})
+
+//     const user = await User.findById(req.params.id);
+
+//     if(user.r)
+// })
