0701092679d9275169002fa9a8c201bb2d41d84e
SantoshThapaa <st708611@gmail.com>
2025-03-25 12:43:03 +0545

    teacher section modified

--- server/controllers/teacher.js
+++ server/controllers/teacher.js
@@ -1,14 +1,20 @@
 import TryCatch from "../middlewares/TryCatch.js";
+import { Courses } from "../models/Courses.js";
+import { Lecture } from "../models/Lecture.js";
+import { rm } from "fs";
+import { promisify } from 'util';
+import fs from 'fs';
+import { User } from "../models/User.js";
 
-export const createCourse = TryCatch(async(req, res)=>{
-    const {title,
+export const createCourse = TryCatch(async (req, res) => {
+    const { title,
         description,
         category,
         createdBy,
         duration,
-        price}= req.body;
+        price } = req.body;
     const image = req.file;
-    await courses.create({
+    await Courses.create({
         title,
         description,
         category,
@@ -23,25 +29,58 @@ export const createCourse = TryCatch(async(req, res)=>{
 });
 
 
-export const addLectures = TryCatch(async(req, res)=>{
+export const addLectures = TryCatch(async (req, res) => {
     const course = await Courses.findById(req.params.id)
-    if(!course)
+    if (!course)
         return res.status(404).json({
-    message: "No course with this id",
+            message: "No course with this id",
+        });
+    const { title, description } = req.body;
+
+    const file = req.file;
+
+    const lecture = await lecture.create({
+        title,
+        description,
+        video: file?.path,
+        course: course._id,
+    });
+
+    return res.status(201).json({
+        message: "Lecture added successfully",
+        lecture,
+    })
 });
-const {title, description }= req.body;
 
-const file = req.file;
-
-const lecture = await lecture.create({
-    title,
-    description,
-    video: file?.path,
-    course: course._id,
+export const deleteLecture = TryCatch(async(req, res)=> {
+    const lecture = await Lecture.findById(req.params.id); 
+    rm(lecture.video, ()=>{
+        console.log("Video deleted");
+    });
+    await lecture.deleteOne()
+    res.json({message: "Lecture Deleted"})
 });
 
-return res.status(201).json({
-    message:"Lecture added successfully",
-    lecture,
-})
-})
+const unlinkAsync = promisify(fs.unlink)
+
+export const deleteCourse = TryCatch(async(req, res)=> {
+    const course = await Courses.findById(req.params.id);
+    const lectures = await Lecture.find({course: course._id})
+    await Promise.all(
+        lectures.map(async(lecture)=> {
+            await unlinkAsync(lecture.video);
+            console.log("Video Deleted");
+        })
+    );
+    rm(course.image, ()=>{
+        console.log("Image deleted");
+    });
+
+    await Lecture.find({course: req.params.id}).deleteMany()
+    await course.deleteOne()
+    await User.updateMany({}, {$pull:{subscription: req.params.id}});
+
+    res.json({
+        message: "Course Deleted",
+    });
+});
