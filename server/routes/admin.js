f35ee41b148d6ee5bee92a06898aded38ad8fc83
SantoshThapaa <st708611@gmail.com>
2025-03-25 12:44:25 +0545

    Admin Section route modified

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { isAdmin, isAuth } from '../middlewares/isAuth.js';
+import { getAllStats } from '../controllers/admin.js';
 
 const router = express.Router();
 
-
+router.get("/stats", isAuth, isAdmin, getAllStats);
 
 export default router;
