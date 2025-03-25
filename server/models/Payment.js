444fa09939e5bcf34b0b9fafdd85abb0cad22faa
SantoshThapaa <st708611@gmail.com>
2025-03-25 13:38:27 +0545

    Payment model created

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -1,5 +1,61 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({
-    
-})
+export const paymentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',  
+        required: true
+    },
+    paymentPlan: {
+        type: String,
+        enum: ['Full Pay', 'EMI'],  
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true  
+    },
+    cardDetails: {
+        cardNumber: {
+            type: String,
+            required: true
+        },
+        expiry: {
+            type: String,  
+            required: true
+        },
+        cvc: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        },
+        postalCode: {
+            type: String,
+            required: true
+        }
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['Pending', 'Completed', 'Failed'],
+        default: 'Pending'
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['Stripe'],  
+        required: true
+    },
+    transactionId: {
+        type: String,  
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const Payment = mongoose.model('Payment', paymentSchema);
+
