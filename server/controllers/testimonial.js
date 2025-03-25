1299e5a553ee6f39b451f6bbecae8065a2603920
SantoshThapaa <st708611@gmail.com>
2025-03-25 13:25:25 +0545

    Testimonial controller created

---
+++ server/controllers/testimonial.js
@@ -0,0 +1,43 @@
+import { Testimonial } from "../models/testimonial.js";
+import { User } from "../models/User.js";
+
+export const createTestimonial = async (req, res) => {
+    try {
+        const { rating, review, profilePic, role } = req.body;
+        const userId = req.user._id; 
+      
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const testimonial = new testimonial({
+            userId,
+            name: user.name,
+            role: role || user.role, 
+            review,
+            rating,
+            profilePic
+        });
+
+        await testimonial.save();
+        res.status(201).json({
+            message: 'Testimonial submitted successfully!',
+            testimonial
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+
+export const getAllTestimonials = async (req, res) => {
+    try {
+        const testimonials = await Testimonial.find().populate('userId', 'name email role');
+        res.status(200).json(testimonials);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
