1501d597001a8cadf1be12ea8a848d471d629f62
SantoshThapaa <st708611@gmail.com>
2025-03-25 13:16:24 +0545

    Testimonial model created

--- server/models/testimonial.js
+++ server/models/testimonial.js
@@ -1,17 +1,37 @@
-import mongoose,{Schema} from "mongoose";
- 
- const testimonialSchema = new Schema({ 
-     content:{
-         required: true,
-         type: String
-     },    
-     rating:{
-         required: true,
-         type: Number
-     },   
-     courseId:{  type: Schema.ObjectId, ref: "Course" },
- 
-     user:{  type: Schema.ObjectId, ref: "User" },
-  
- });
- export const Testimonial = mongoose.models.Testimonial ?? mongoose.model("Testimonial",testimonialSchema);
+const mongoose = require('mongoose');
+
+const testimonialSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',  
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    review: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    profilePic: {
+        type: String, 
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Testimonial', testimonialSchema);
