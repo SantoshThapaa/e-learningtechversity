87cf0aea8488c2e09c21afdaa68d81b2d35eaa1e
SantoshThapaa <st708611@gmail.com>
2025-03-25 11:17:42 +0545

    Multer added

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async() => {
     try {
-        await mongoose.connect( process.env.MONGODB_CONNECTION_STRING);
+        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
         console.log("Connected to MongoDB");
     } catch(error){
         console.log(error);
