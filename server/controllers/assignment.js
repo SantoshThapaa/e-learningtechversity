bfa48afe8498ed9591bdd1051a3a4f0ca16cf2a5
SantoshThapaa <st708611@gmail.com>
2025-03-26 12:33:02 +0545

    Assignment controller created

---
+++ server/controllers/assignment.js
@@ -0,0 +1,28 @@
+import { Assignment } from "../models/Assignment";
+import { Courses } from "../models/Courses";
+
+export const postAssignment = TryCatch(async (req, res) => {
+    const { courseId } = req.params;
+    const { title, description, deadline } = req.body;
+    const teacherId = req.user._id;
+
+    const course = await Courses.findById(courseId);
+    if (!course) return res.status(404).json({ message: "Course not found" });
+
+    if (!course.assignedTeachers.includes(teacherId.toString())) {
+        return res.status(403).json({ message: "You are not assigned to this course" });
+    }
+
+    const assignment = await Assignment.create({
+        title,
+        description,
+        deadline,
+        course: course._id,
+        createdBy: teacherId,
+    });
+
+    res.status(201).json({
+        message: "Assignment posted successfully",
+        assignment,
+    });
+});
