9430e14e0c1d36f86ba3c2044c6f1462326967dc
SantoshThapaa <st708611@gmail.com>
2025-03-26 12:31:46 +0545

    Teacher route modified

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import { isAdmin, isAuth, isTeacher } from '../middlewares/isAuth.js';
-import { addLectures, createCourse, deleteCourse, deleteLecture } from '../controllers/teacher.js';
+import { addLectures, createCourse, deleteCourse, deleteLecture, teacherJoinCourse } from '../controllers/teacher.js';
 import { uploadFiles } from '../middlewares/multer.js';
+import { postAssignment } from '../controllers/assignment.js';
+import { getEnrolledStudents } from '../controllers/course.js';
 
 const router = express.Router();
 
 router.post("/course/new", isAuth, isTeacher, uploadFiles, createCourse);
-router.post("/course/:id", isAuth, isAdmin, uploadFiles, addLectures);
-router.delete("/course/:id", isAuth, isAdmin, deleteCourse);
-router.delete("/lecture/:id", isAuth, isAdmin, deleteLecture);
+router.post("/course/:id", isAuth, isTeacher, uploadFiles, addLectures);
+router.delete("/course/:id", isAuth, isTeacher, deleteCourse);
+router.delete("/lecture/:id", isAuth, isTeacher, deleteLecture);
+router.post("/course/:courseId/join", isAuth, isTeacher, teacherJoinCourse);
+router.post("/course/:courseId/assignment", isAuth, isTeacher, postAssignment);
+router.get("/course/:courseId/students", isAuth, isTeacher, getEnrolledStudents);
 
 export default router;
