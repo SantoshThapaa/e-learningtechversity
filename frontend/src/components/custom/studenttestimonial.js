ebabebb3b98be98dd79d0ae394e00e6ed458fd7a
SantoshThapaa <st708611@gmail.com>
2025-03-28 12:52:41 +0545

    Student testimonial added

---
+++ frontend/src/components/custom/studenttestimonial.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const studenttestimonial = () => {
+    return (
+        <div className='bg-[#F6FFEB] '>
+            <div className='font-unbounded semibold'>
+                In Their Own Words:
+                Student Testimonials
+            </div>
+        </div>
+    )
+}
+
