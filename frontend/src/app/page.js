9a126364d753610d3aaad585f3c9d5001d7cad9a
SantoshThapaa <st708611@gmail.com>
2025-03-29 21:43:22 +0545

    Modified

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,27 +1,10 @@
-import AboutSection from '@/components/custom/about'
-import Contact from '@/components/custom/contact'
-import FeaturesSection from '@/components/custom/featureSection'
-import Footer from '@/components/custom/Footer/StudentFooter'
-import HeroBanner from '@/components/custom/Hero'
-import LogoContent from '@/components/custom/logoContent'
-import TeamSection from '@/components/custom/teamSection'
-import { Testimonial } from '@/components/custom/testimonial'
-import React from 'react'
+import React from 'react';
+import Home from './student/home/page';
 
-const StudentDashboard = () => {
+export const page = () => {
   return (
     <>
-    <HeroBanner/>
-    <AboutSection/>
-    <LogoContent/>
-    <FeaturesSection/>
-    <Testimonial/>
-    <TeamSection/>
-    <Contact/>
-    <Footer/>
-    
+     <Home/>
     </>
-  )
-}
-
-export default StudentDashboard
+  );
+};
