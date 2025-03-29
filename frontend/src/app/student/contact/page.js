5fe95885874621abcb416642fec6a478d0dbf0ce
SantoshThapaa <st708611@gmail.com>
2025-03-29 21:36:09 +0545

    Contact folder created

---
+++ frontend/src/app/student/contact/page.js
@@ -0,0 +1,16 @@
+import ContactForm from '@/components/custom/ContactForm'
+import CourseBanner from '@/components/custom/CourseBanner'
+import CourseIntro from '@/components/custom/CourseIntro'
+import Footer from '@/components/custom/Footer/StudentFooter'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+    <CourseBanner/>
+    <ContactForm/>
+    <Footer/>
+    </>
+  )
+}
+export default Contact;
