fc9ad8e8e0750fe660a215f8e66d1066c5533d44
SantoshThapaa <st708611@gmail.com>
2025-03-31 21:59:30 +0545

    route modified

--- server/routes/testimonial.js
+++ server/routes/testimonial.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createTestimonial, getAllTestimonials } from '../controllers/testimonial.js';
+import { isAuth } from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
-router.post('/', createTestimonial);
+router.post('/createTestimonial', isAuth, createTestimonial);
 router.get('/', getAllTestimonials);
 
 export default router;
