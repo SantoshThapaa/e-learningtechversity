42b408ad738b8edf1b26987301140161f36ced1f
SantoshThapaa <st708611@gmail.com>
2025-03-31 22:20:23 +0545

    modified

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from 'cors';
 import { connectDb } from './database/db.js';
 //importing routes
 import userRoutes from "./routes/user.js";
@@ -9,11 +10,14 @@ import adminRoutes from "./routes/admin.js";
 import testimonialRoutes from "./routes/testimonial.js";
 import paymentRoutes from "./routes/payment.js";
 import resourceRoutes from "./routes/resource.js";
+import messageRoutes from "./routes/message.js";
+
 
 const app= express();
 
 dotenv.config();
-
+// Enable CORS for all routes
+app.use(cors());
 //using middlewares
 app.use(express.json());
 const port =4000;
@@ -30,6 +34,7 @@ app.use("/api", adminRoutes);
 app.use("/api", testimonialRoutes);
 app.use('/api/', paymentRoutes);
 app.use('/api/', resourceRoutes);
+app.use('/api/', messageRoutes);
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
