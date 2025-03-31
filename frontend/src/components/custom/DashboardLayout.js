e59e356321852df59e09a5c1594760db27b91ff8
SantoshThapaa <st708611@gmail.com>
2025-03-31 22:01:23 +0545

    Dashboard Layout added

---
+++ frontend/src/components/custom/DashboardLayout.js
@@ -0,0 +1,19 @@
+
+import AdminRightSidebar from './AdminRightSidebar';
+
+
+const DashboardLayout = ({ children }) => (
+    <div className="flex">        
+        {/* Main Content Area */}
+        <div className="flex-1 p-10 bg-gray-100 ml-64">
+            {children}
+        </div>
+        
+        {/* Admin Right Sidebar (Calendar and Assignments) */}
+        <div className="w-80 h-screen fixed right-0 top-0 p-4 bg-white shadow-lg">
+            <AdminRightSidebar />
+        </div>
+    </div>
+);
+
+export default DashboardLayout;
