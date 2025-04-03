2ae61178a6d7761af190dcff148978dfeda17fd3
SantoshThapaa <st708611@gmail.com>
2025-04-03 21:38:16 +0545

    Modified

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginUser, myProfile, register, verifyUser } from '../controllers/user.js';
+import { loginUser, logoutUser, myProfile, register, verifyUser } from '../controllers/user.js';
 import { isAuth } from '../middlewares/isAuth.js';
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/user/register", register);
 router.post("/user/verify", verifyUser);
 router.post("/user/login", loginUser);
+router.post("/user/logout", logoutUser);
 router.get("/user/me", isAuth, myProfile);
 
 export default router;
