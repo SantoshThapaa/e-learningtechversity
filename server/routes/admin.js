c476c74ce898bf68a69f3576bfa4c8bf188f240b
SantoshThapaa <st708611@gmail.com>
2025-04-09 23:36:07 +0545

    Admin Route modified

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { isAdmin, isAuth, isTeacher } from '../middlewares/isAuth.js';
 import { assignTeacherToCourse, createCourse, getAllCourses, getAllStats, getAllStudents, getAllTeachers, registerTeacher, Teacherlogin } from '../controllers/admin.js';
-import { uploadFiles } from '../middlewares/multer.js';
+import { uploadImage } from '../middlewares/multer.js';
 
 const router = express.Router();
 
@@ -10,7 +10,7 @@ router.post("/registerteacher", registerTeacher);
 router.get("/students", getAllStudents);
 router.post("/assign-teacher", assignTeacherToCourse);
 router.get('/teachers', getAllTeachers);
-router.post('/createnewcourses',uploadFiles, createCourse);
+router.post('/createnewcourses',uploadImage, createCourse);
 router.get('/allcourses', getAllCourses);
 router.post("/login", Teacherlogin);
 router.post("/assign-teacher",isTeacher, assignTeacherToCourse);
