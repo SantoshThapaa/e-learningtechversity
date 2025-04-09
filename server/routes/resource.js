1c534b7f60df6283447f8a446ef3863ce24bf5a8
SantoshThapaa <st708611@gmail.com>
2025-04-09 23:35:30 +0545

    Resourse.js modified

--- server/routes/resource.js
+++ server/routes/resource.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { deleteResource, getCourseResources, uploadResource } from "../controllers/resource.js";
 import { isAuth } from "../middlewares/isAuth.js";
-import { uploadFiles } from "../middlewares/multer.js";
+import { uploadDocument } from '../middlewares/multer.js';
 
 
 const router = express.Router();
 
-router.post("/course/:courseId/resource", uploadFiles, isAuth, uploadResource);
+router.post("/course/:courseId/resource", uploadDocument, isAuth, uploadResource);
 router.get("/course/:courseId/resources", isAuth, getCourseResources);
 router.delete("/resource/:id", isAuth, deleteResource);
 
