3edb76d6fb2a20504e6336b22b8c04e8cac6ec4e
SantoshThapaa <st708611@gmail.com>
2025-04-12 22:56:35 +0545

    User route modified

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { deleteUserProfile, editUserProfile, loginUser, logoutUser, myProfile, register, verifyUser, viewUserProfile } from '../controllers/user.js';
+import { deleteUserProfile, getLecturesByCourse, loginUser, logoutUser, myProfile, register, updateUserProfile, verifyUser, viewUserProfile } from '../controllers/user.js';
 import { isAuth } from '../middlewares/isAuth.js';
+import { uploadImage } from '../middlewares/multer.js';
 
 const router = express.Router();
 
@@ -9,8 +10,10 @@ router.post("/user/verify", verifyUser);
 router.post("/user/login", loginUser);
 router.post("/user/logout", logoutUser);
 router.get("/user/me", isAuth, myProfile);
-router.get('/user/:userId', viewUserProfile);  
-router.put('/user/:userId', editUserProfile);  
+router.put('/profile', isAuth, uploadImage, updateUserProfile);
+router.get('/user/:userId', viewUserProfile);
 router.delete('/user/:userId', deleteUserProfile);
+router.get('/lectures/:courseId', isAuth, getLecturesByCourse);
+
 
 export default router;
