e48306ff47cb09b00ede0c8b8e3a4645028351d8
SantoshThapaa <st708611@gmail.com>
2025-04-12 22:38:17 +0545

    Profile schema created

---
+++ server/models/Profile.js
@@ -0,0 +1,33 @@
+// models/schemas/profileSchema.js
+import mongoose from 'mongoose';
+
+const profileSchema = new mongoose.Schema({
+  profilePicture: {
+    type: String,
+    required: false,
+  },
+  coverImage: {
+    type: String,
+    required: false,
+  },
+  bio: {
+    type: String,
+    required: false,
+  },
+  zipCode: {
+    type: String,
+    required: false,
+  },
+  socialMedia: {
+    type: Map,
+    of: String,
+  },
+  subscription: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+    },
+  ],
+}, { _id: false });
+
+export default profileSchema;
