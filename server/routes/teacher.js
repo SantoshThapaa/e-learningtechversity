4cf607399a3f9ca581d46091913381283aa8811c
SantoshThapaa <st708611@gmail.com>
2025-04-12 22:56:10 +0545

    teacher route modified

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { isAuth, isTeacher } from '../middlewares/isAuth.js';
 import { createLecture, deleteLecture, getAssignedCourses} from '../controllers/teacher.js';
-import { uploadCourseFiles } from '../middlewares/multer.js';
 import { postAssignment } from '../controllers/assignment.js';
 import { getEnrolledStudents } from '../controllers/course.js';
+import { uploadThumbnail } from '../middlewares/multer.js';
 
 const router = express.Router();
 
-router.post("/course/:courseId/lecture", isAuth, isTeacher, uploadCourseFiles, createLecture);
+router.post("/course/:courseId/lecture", isAuth, isTeacher, uploadThumbnail, createLecture);
 router.delete("/lecture/:id", isAuth, isTeacher, deleteLecture);
 router.post("/course/:courseId/assignment", isAuth, isTeacher, postAssignment);
 router.get("/course/:courseId/students", isAuth, isTeacher, getEnrolledStudents);
