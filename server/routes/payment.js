4ea708b1e7ee2b5712f1c4c04511e58055d8bad0
SantoshThapaa <st708611@gmail.com>
2025-04-12 22:55:45 +0545

    payment route modified

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import { createPayment, getPayments } from '../controllers/payment.js';
-import { isAuth } from '../middlewares/isAuth.js';
+import { isAdmin, isAuth } from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
-
-router.post('/createPayment', isAuth, createPayment);
-router.get('/getPayment', isAuth, getPayments);
+router.post('/createpayments', isAuth, createPayment);
+router.get('/getallpayments', isAuth, isAdmin, getPayments);
 
 export default router;
