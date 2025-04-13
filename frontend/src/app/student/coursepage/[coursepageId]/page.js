dc56699746959cb6eff8b66af5d5df8d52afee21
SantoshThapaa <st708611@gmail.com>
2025-04-13 13:59:18 +0545

    Coursepage modified

---
+++ frontend/src/app/student/coursepage/[coursepageId]/page.js
@@ -0,0 +1,16 @@
+import InstructorCard from "@/components/custom/instructorcard";
+import StudentNavbar from "@/components/custom/navbar/StudentNavbar";
+
+export default function CoursePage() {
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      <StudentNavbar />
+
+      {/* Make InstructorCard full width */}
+      <div className="-mx-4 sm:-mx-6 lg:-mx-8">
+        <InstructorCard />
+      </div>
+    </div>
+  );
+}
+
