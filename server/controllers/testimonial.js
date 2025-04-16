f83fff653209c5f58e79aea73b2a7aea5452ea4d
SantoshThapaa <st708611@gmail.com>
2025-04-16 10:36:27 +0545

    testimonial controller changed

--- server/controllers/testimonial.js
+++ server/controllers/testimonial.js
@@ -2,55 +2,45 @@ import { Testimonial } from "../models/testimonial.js";
 import { User } from "../models/User.js";
 
 export const createTestimonial = async (req, res) => {
-    try {
-        const { rating, review, profilePic, role } = req.body;
-        console.log(req.body);
+  try {
+    const { review, rating, role, name } = req.body;
+    const profilePic = req.file ? req.file.path : '';
 
-        const userId = req.user._id;
-
-        console.log(userId);
-        if (!review || !rating || !profilePic) {
-            return res.status(400).json({ message: 'Review, rating, and profilePic are required.' });
-        }
-
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        const testimonial = new Testimonial({
-            userId,
-            name: user.name,
-            role: role || user.role,
-            review,
-            rating,
-            profilePic
-        });
-
-        await testimonial.save();
-        res.status(201).json({
-            message: 'Testimonial submitted successfully!',
-            testimonial
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
+    if (!review || !rating || !profilePic) {
+      return res.status(400).json({ message: 'Review, rating, and profilePic are required.' });
     }
+
+    const testimonial = new Testimonial({
+      name: name || 'User',
+      role: role || 'Student',
+      review,
+      rating,
+      profilePic,
+    });
+
+    await testimonial.save();
+
+    res.status(201).json({
+      message: 'Testimonial submitted successfully!',
+      testimonial,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
 };
-
-
 
 export const getAllTestimonials = async (req, res) => {
     try {
-        const testimonials = await Testimonial.find().populate(
-            'userId',
-             'name email role'
-            );
-        res.status(200).json(testimonials);
+      const testimonials = await Testimonial.find().limit(4);  
+      res.status(200).json(testimonials);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({
-             message: 'Server error' 
-        });
+      console.error(error);
+      res.status(500).json({
+        message: 'Server error',
+      });
     }
-};
+  };
+  
+
+
