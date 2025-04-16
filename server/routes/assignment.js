dd36e78790e7cc6344750202fb1e1cc1bdde6cdd
SantoshThapaa <st708611@gmail.com>
2025-04-16 15:41:50 +0545

    Assignment route modified

--- server/routes/assignment.js
+++ server/routes/assignment.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getSubmissionsForAssignment, getTeacherAssignments, postAssignment, submitAssignment } from "../controllers/assignment.js";
+import { getAssignmentsByCourse, getSubmissionsForAssignment, getTeacherAssignments, postAssignment, submitAssignment } from "../controllers/assignment.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 
@@ -8,5 +8,6 @@ router.post("/create/:courseId", isAuth, postAssignment);
 router.post("/submit/:assignmentId", isAuth, submitAssignment);
 router.get("/submissions/:assignmentId", isAuth, getSubmissionsForAssignment);
 router.get("/courseassignments", isAuth, getTeacherAssignments);
+router.get("/teacher/assignments/course/:courseId",getAssignmentsByCourse);
 
 export default router;
