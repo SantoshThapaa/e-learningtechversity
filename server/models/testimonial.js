50f16b4213265d1d4596e2d1a36a4e9f3f1ae2fd
SantoshThapaa <st708611@gmail.com>
2025-04-16 10:39:36 +0545

    Testimonial model added

--- server/models/testimonial.js
+++ server/models/testimonial.js
@@ -1,11 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
 const testimonialSchema = new Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',  
-        required: true
-    },
     name: {
         type: String,
         required: true
