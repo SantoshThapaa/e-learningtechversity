5d26a7996d7c429e0d845d60bc7cde298327f2ab
SantoshThapaa <st708611@gmail.com>
2025-04-17 12:38:26 +0545

    added

--- frontend/src/app/admin/updates/page.js
+++ frontend/src/app/admin/updates/page.js
@@ -1,12 +1,13 @@
 
 import AdminBlog from '@/components/custom/AdminBlog';
 import AdminTestimonial from '@/components/custom/AdminTestimonial';
+import AdminSidebar from '@/components/custom/navbar/AdminSidebar';
 import React from 'react'
 
 const Page = () => {
   return (
     <div className="flex">
-      <AdminSidebar />
+      <AdminSidebar/>
       <main className="ml-60 w-full p-6 bg-gray-100 min-h-screen">
         <AdminTestimonial/>
         <AdminBlog/>
