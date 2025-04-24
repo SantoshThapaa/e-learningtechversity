5995dd19568c5cc88c29c8109ee39d754013180d
SantoshThapaa <st708611@gmail.com>
2025-04-24 21:58:30 +0545

    Testimonial modified

--- server/routes/testimonial.js
+++ server/routes/testimonial.js
@@ -1,11 +1,13 @@
 
 import express from 'express';
-import { createTestimonial, getAllTestimonials } from '../controllers/testimonial.js';
+import { createTestimonial, deleteTestimonial, editTestimonial, getAllTestimonials } from '../controllers/testimonial.js';
 import { uploadImage } from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.post('/createTestimonial',uploadImage, createTestimonial);
 router.get('/allTestimonials', getAllTestimonials);
+router.put('/editTestimonial/:testimonialId', uploadImage, editTestimonial);
+router.delete('/deleteTestimonial/:testimonialId', deleteTestimonial);
 
 export default router;
