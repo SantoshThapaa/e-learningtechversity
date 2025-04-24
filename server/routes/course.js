33b65bca9df967274556a3139fc7febdf62bcfba
SantoshThapaa <st708611@gmail.com>
2025-04-24 21:59:28 +0545

    course modified

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import { getAllCourses, getSingleCourse, fetchLectures, fetchLecture, getMyCourses } from '../controllers/course.js';
+import { getAllCourses, getSingleCourse,  getMyCourses, getLecturesByCourse, getNewBatchNo, getCoursesByCategory, getAllCategories } from '../controllers/course.js';
 import { isAuth } from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
 router.get("/course/all", getAllCourses);
 router.get("/course/:id", getSingleCourse);
-router.get("/lectures/:id", isAuth, fetchLectures);
-router.get("/lecture/:id", isAuth, fetchLecture);
+router.get("/lectures/:id", isAuth, getLecturesByCourse);
 router.get("/mycourse", isAuth, getMyCourses);
+router.get('/category/:category', getCoursesByCategory);
+router.get('/batch/no', getNewBatchNo);
+router.get('/categories', getAllCategories);
+
 
 export default router;
