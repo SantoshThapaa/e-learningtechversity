e8f5bd48dbb15848c77d0a2c09d9c188e4dae250
SantoshThapaa <st708611@gmail.com>
2025-04-24 21:58:02 +0545

    scheduler modified

--- server/scheduler.js
+++ server/scheduler.js
@@ -1,5 +1,8 @@
 import cron from 'node-cron';
 import Notification from './models/Notification.js';
+import { Payment } from './models/Payment.js';
+import { User } from './models/User.js';
+import sendMail from './middlewares/sendMail.js';  
 
 export const startNotificationScheduler = () => {
   cron.schedule('* * * * *', async () => {
@@ -15,6 +18,39 @@ export const startNotificationScheduler = () => {
 
       note.sent = true;
       await note.save();
+
+      
+      sendMail(note.userEmail, 'Scheduled Notification', {
+        message: note.message,
+      });
+    }
+
+  
+    const payments = await Payment.find({ paymentPlan: 'EMI', accessGranted: true });
+
+    for (const payment of payments) {
+      const today = new Date();
+      const nextPaymentDue = new Date(payment.nextPaymentDue);
+
+      if (today > nextPaymentDue && payment.accessGranted) {
+        const user = await User.findById(payment.userId);
+        if (user) {
+          const notificationMessage = `Your EMI payment is due. Please make the payment to retain access to your course.`;
+          const notification = new Notification({
+            userEmail: user.email,  
+            message: notificationMessage,
+            sendAt: new Date(),  
+          });
+
+          await notification.save();
+          sendMail(user.email, 'EMI Payment Reminder', {
+            name: user.name,
+            message: notificationMessage,
+          });
+        }
+        payment.accessGranted = false;
+        await payment.save();
+      }
     }
   });
 };
