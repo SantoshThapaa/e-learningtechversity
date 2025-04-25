72fd301029bbcfb103afdba7bac5715298e5584b
SantoshThapaa <st708611@gmail.com>
2025-04-25 21:19:46 +0545

    routes modified

--- server/routes/assignment.js
+++ server/routes/assignment.js
@@ -1,14 +1,18 @@
 import express from "express";
-import { getAllAssignments, getAssignmentsByCourse, getSubmissionsForAssignment, getTeacherAssignments, postAssignment, submitAssignment } from "../controllers/assignment.js";
+import { uploadDocument } from '../middlewares/multer.js'; 
+import { checkStatus, getAllAssignments, getAssignmentsByCourse, getSubmissionsForAssignment, getSubmissionStatus, getTeacherAssignments, postAssignment, submitAssignment, updateSubmissionStatus } from "../controllers/assignment.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 
 const router = express.Router();
 router.post("/create/:courseId", isAuth, postAssignment);
-router.post("/submit/:assignmentId", isAuth, submitAssignment);
+router.post("/submit/:assignmentId", isAuth, uploadDocument, submitAssignment);
 router.get("/submissions/:assignmentId", isAuth, getSubmissionsForAssignment);
 router.get("/courseassignments", isAuth, getTeacherAssignments);
 router.get("/teacher/assignments/course/:courseId",getAssignmentsByCourse);
 router.get("/assignments", getAllAssignments);
+router.get('/status/:assignmentId', isAuth, checkStatus);
+router.get('/assignment/status/:assignmentId', isAuth, getSubmissionStatus);
+router.post('/assignment/status/update/:assignmentId', isAuth, updateSubmissionStatus);
 
 export default router;
