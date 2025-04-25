6dbaf99e347ca28d715b4f0f2576f61d78fdd31e
SantoshThapaa <st708611@gmail.com>
2025-04-25 21:24:58 +0545

    Assignment submission added

---
+++ frontend/src/app/student/assignmentsubmission/[assignmentId]/page.js
@@ -0,0 +1,16 @@
+import AssignmentInstructorCard from "@/components/custom/assignmentInstructorCard";
+import StudentNavbar from "@/components/custom/navbar/StudentNavbar";
+import AssignmentSubmissionTab from "@/components/custom/tabs/AssignmentSubmissionTab";
+
+export default function CoursePage() {
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      <StudentNavbar />
+      <div className="-mx-4 sm:-mx-6 lg:-mx-8">
+        <AssignmentInstructorCard/>
+        <AssignmentSubmissionTab/>
+      </div>
+    </div>
+  );
+}
+
