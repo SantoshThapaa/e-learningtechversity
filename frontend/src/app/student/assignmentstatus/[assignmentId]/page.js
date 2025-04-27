44beffafeb2ed797720360ff71015cd00e0c43ce
SantoshThapaa <st708611@gmail.com>
2025-04-27 21:49:36 +0545

    added

--- frontend/src/app/student/assignmentstatus/[assignmentId]/page.js
+++ frontend/src/app/student/assignmentstatus/[assignmentId]/page.js
@@ -1,15 +1,20 @@
+"use client"
+import { useParams } from "next/navigation";
 import AssignmentInstructorCard from "@/components/custom/assignmentInstructorCard";
 import StudentNavbar from "@/components/custom/navbar/StudentNavbar";
 import AssignmentStatusTab from "@/components/custom/tabs/AssignmentStatusTab";
 
 
-export default function CoursePage() {
+export default function TaskSubmissionPage() {
+  const { assignmentId } = useParams();
   return (
     <div className="max-w-screen-lg mx-auto">
       <StudentNavbar />
       <div className="-mx-4 sm:-mx-6 lg:-mx-8">
-        <AssignmentInstructorCard/>
-        <AssignmentStatusTab/>
+        <AssignmentInstructorCard />
+        <div className="p-8">
+          <AssignmentStatusTab assignmentId={assignmentId} />
+        </div>
       </div>
     </div>
   );
