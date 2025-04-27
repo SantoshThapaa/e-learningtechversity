dbd6f744f2b5bfa389241a57d39e733ace5670a5
SantoshThapaa <st708611@gmail.com>
2025-04-27 21:48:30 +0545

    assignment route modified

--- server/routes/assignment.js
+++ server/routes/assignment.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { uploadDocument } from '../middlewares/multer.js'; 
-import { checkStatus, getAllAssignments, getAssignmentsByCourse,  getSingleStudentSubmissionsForTeacher,  getSubmissionsForAssignment, getSubmissionStatus, getTeacherAssignments, postAssignment, submitAssignment, updateSubmissionStatus } from "../controllers/assignment.js";
+import { checkStatus, getAllAssignments, getAssignmentsByCourse,  getSingleSubmissionOfStudentForAssignment,  getSubmissionsForAssignment, getSubmissionStatus, getSubmissionStatusForStudent, getTeacherAssignments, postAssignment, submitAssignment, submitFeedbackForAssignmentAndStudent} from "../controllers/assignment.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 
@@ -13,10 +13,8 @@ router.get("/teacher/assignments/course/:courseId",getAssignmentsByCourse);
 router.get("/assignments", getAllAssignments);
 router.get('/status/:assignmentId', isAuth, checkStatus);
 router.get('/assignment/status/:assignmentId', isAuth, getSubmissionStatus);
-router.post('/assignment/status/update/:assignmentId', isAuth, updateSubmissionStatus);
-router.get("/teacher/submissions/student/:studentId", isAuth, getSingleStudentSubmissionsForTeacher);
-
-
-
+router.post('/feedback/:assignmentId/:studentId', isAuth, submitFeedbackForAssignmentAndStudent);
+router.get("/submission/:assignmentId/:studentId", isAuth, getSingleSubmissionOfStudentForAssignment);
+router.get('/student/submission-status/:assignmentId', isAuth, getSubmissionStatusForStudent);
 
 export default router;
