e2063da8e768b8df50ce5abb18320a0db5f5e517
SantoshThapaa <st708611@gmail.com>
2025-05-02 17:17:41 +0545

    image section problem solved

--- frontend/src/app/student/blog/[blogId]/page.js
+++ frontend/src/app/student/blog/[blogId]/page.js
@@ -1,4 +1,5 @@
 import BlogPost from "@/components/custom/BlogPost";
+import Footer from "@/components/custom/Footer/StudentFooter";
 import StudentNavbar from "@/components/custom/navbar/StudentNavbar";
 
 export default function CoursePage() {
@@ -6,9 +7,9 @@ export default function CoursePage() {
     <div className="max-w-screen-lg mx-auto">
       <StudentNavbar />
       <div className="-mx-4 sm:-mx-6 lg:-mx-8">
-        <BlogPost/>
+        <BlogPost />
       </div>
+      <Footer className="w-full" />
     </div>
   );
 }
-
