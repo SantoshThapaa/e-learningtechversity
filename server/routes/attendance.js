2baf70ca4c6b6db591a4920cd9ae46dd85b1b675
SantoshThapaa <st708611@gmail.com>
2025-05-02 17:15:41 +0545

    Attendance route modified

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { getStudentAttendance, getStudentsByCourse, saveAttendance } from "../controllers/attendance.js";
+import { getStudentAttendance, getStudentAttendancePercentage, getStudentsByCourse, saveAttendance } from "../controllers/attendance.js";
 
 const router = express.Router();
 
 router.get("/courses/:courseId/students", isAuth, getStudentsByCourse);
 router.post("/attendance/:courseId/save", isAuth, saveAttendance);
 router.get("/attendance/student", isAuth, getStudentAttendance);
+router.get('/courses/:courseId/attendance/percentage',isAuth, getStudentAttendancePercentage);
 
 export default router;
